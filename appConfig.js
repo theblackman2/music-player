9b893f9ba7aa44d923390e6d6881508bee186d58
theblackman <104510010+theblackman2@users.noreply.github.com>
2022-08-19 17:06:56 +0100

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,3 +1,3 @@
 export const CLIENT_ID = "fc0e57b055a04692bf3badb95aeb77f1";
-export const REDIRECT_URI = "http://localhost:5173/login";
+export const REDIRECT_URI = "https://spot-music.vercel.app/login";
 export const API_URL = "https://accounts.spotify.com/authorize";
